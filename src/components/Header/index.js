c6903b98f7089eac94be4ef5b5b77e9f2cae6acb
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2020-10-09 05:59:09 -0300

    Create React App

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles.js';
+
+function Header() {
+  return (
+  <Container>
+    <h1>Jobfy</h1>
+  </Container>
+  );
+};
+
+export default Header;
